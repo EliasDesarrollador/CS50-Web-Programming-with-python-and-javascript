0846dfea3e8df0fdbcf6f2431f09eba6fd6d5065
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-09-06 13:54:31 -0300

    Variable, y estructura de datos Ejemplos de ejercicios en python

--- Python/secuencias.py
+++ Python/secuencias.py
@@ -1,9 +1,11 @@
 
 #Ejemplo 4 
+#ESTRUCTURA DE DATOS 
 #Secuencias 
+#Listas
 name = "Elias "
- # print(name[0])  # Imprimos el primer elemento del la lista 
+print(name[0])  # Imprimos el primer elemento del la lista 
 
 names = ["Elias", "Juan", "Luis "]   # Cargamos un array  de nombres 
-#print(names ) # imprimimos el array completo 
+print(names ) # imprimimos el array completo 
 print (names[0]) #imprimos el primer elemento del array names 
---
+++ Python/secuencias2.py
@@ -0,0 +1,9 @@
+
+# Ejemplo 4
+#Secuencias 
+#Tupla 
+
+coordenadaX = 10.0
+coodeanadaY = 20.0
+
+coodenada = (10.0 , 20.0)
