a0f0f3c5d45c2f82d682bbecddb9619ac90011fe
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-09-06 13:18:07 -0300

    Subir Contenido de Curso de CS50

---
+++ Python/condicionales.py
@@ -0,0 +1,10 @@
+
+n   =   int   (input ("Numero : "))
+
+
+if  n  >  0 :
+        print("n es  positivo ")
+elif   n  <  0:
+        print("n  es negativo")
+else:
+        print("n  es cero")
---
+++ Python/name.py
@@ -0,0 +1,3 @@
+
+name  = input ("Tu nombre:  ")
+print (f"Hola !! ,  {name}")
