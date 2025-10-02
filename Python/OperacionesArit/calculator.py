1a0e3d7b295019c6e98155ecbbe74336de121bbd
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-10-02 16:17:47 -0300

    Funciones  ejercicios

--- Python/OperacionesArit/calculator.py
+++ Python/OperacionesArit/calculator.py
@@ -3,10 +3,10 @@
 x = input ("Valor de  x : ")
 y = input ("Valor de y : ")
 #Operacion de suma 
-z  = int(x) + int(y)
+z  = float(x) + float(y)
 #Operacion de resta 
-r = int (x)  - int(y)
+# r = int (x)  - int(y)
 
 #Mostramos el resultado
 print(f"El resultado de la suma es : {z}")
-print(f"El resultado de la resta es : {r}")
+# print(f"El resultado de la resta es : {r}")
---
+++ Python/Variables-Funciones/funciones.py
@@ -0,0 +1,6 @@
+
+def hello():
+    print("Hola !!  ")
+name = input ("Cual es tu nombre  ? : ")
+hello()
+print(name)
