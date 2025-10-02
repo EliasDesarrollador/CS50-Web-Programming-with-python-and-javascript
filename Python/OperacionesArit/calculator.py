8e7b08ea15a930c0b1fea77c3b7bfde553625865
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-10-02 12:35:46 -0300

    Operaciones aritmeticas

--- Python/OperacionesArit/calculator.py
+++ Python/OperacionesArit/calculator.py
@@ -2,9 +2,11 @@
 # Valores de entrada que pedimos al usuario 
 x = input ("Valor de  x : ")
 y = input ("Valor de y : ")
-
 #Operacion de suma 
 z  = int(x) + int(y)
+#Operacion de resta 
+r = int (x)  - int(y)
 
 #Mostramos el resultado
 print(f"El resultado de la suma es : {z}")
+print(f"El resultado de la resta es : {r}")
